5416c4e7bf570ca3fa566222a6f206b6f1fe5eef
purnami <purnami.indryaswari@gmail.com>
2024-11-05 12:31:29 +0800

    Add realm package

--- Todoey/AppDelegate.swift
+++ Todoey/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import RealmSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -19,6 +20,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         
 //        print(NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).last! as String)
+        print(Realm.Configuration.defaultConfiguration.fileURL)
+        
+        let data = Data()
+        data.name = "mimi"
+        data.age = 2
+        
+        do {
+            let realm = try Realm()
+            try realm.write {
+                realm.add(data)
+            }
+        } catch {
+            print("Error initializing new Realm \(error)")
+        }
+        
+        
         return true
     }
 
